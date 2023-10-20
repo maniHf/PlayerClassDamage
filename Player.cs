c9de62aeca7accf611bcea5f16d8c87dc9fa83ae
OLDSPYDERMAN <112350245+maniHf@users.noreply.github.com>
2023-10-20 20:14:58 +0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PlayerClass
+{
+    public class Player
+    {
+        private int Health = 100;
+        public int s_Health
+        {
+            get { return Health; }
+            set
+            { if (value >= 0 && value < 100) Health = value; }
+        }
+        public void m_Hit (int Damage){this.Health -= Damage;}
+     
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+/* Manihf Govnokod
+ * s_ свойства данных 
+ * m_ метод 
+ * с_ класс 
+ * 
+ * 
+ * 
+ * 
+ */
+ 
+using System;
+using PlayerClass;
+
+namespace Manihf
+{
+    class Program
+    {
+        public static Player player = new Player();
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hit-Health");
+            player.m_Hit(23);
+            Console.WriteLine(player.s_Health);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+// РЕШИТЬ ПРОБЛЕМУ С ЗДОРОВЬЕМ 
